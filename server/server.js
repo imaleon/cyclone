01f8a03560335696032b759c23913b2382180204
Aleon <aneloaleon@gmail.com>
2026-05-19 10:32:02 +0800

    Refactor server.js for match finding functionality

--- server/server.js
+++ server/server.js
@@ -17,7 +17,6 @@ const io = new Server(server, {
 
 const waitingPlayers = [];
 const rooms = {};
-const players = {};
 
 function createRoomId() {
     return Math.random()
@@ -28,10 +27,60 @@ function createRoomId() {
 
 io.on('connection', socket => {
 
-    console.log('CONNECTED', socket.id);
+    console.log('CONNECTED:', socket.id);
 
-    players[socket.id] = {
-        id: socket.id,
-        username: `Player_${socket.id.slice(0,4)}`
-    };
+    socket.on('findMatch', () => {
+
+        if (waitingPlayers.length > 0) {
+
+            const opponentId = waitingPlayers.shift();
+
+            const roomId = createRoomId();
+
+            socket.join(roomId);
+
+            const opponentSocket =
+                io.sockets.sockets.get(opponentId);
+
+            if (!opponentSocket) return;
+
+            opponentSocket.join(roomId);
+
+            rooms[roomId] = {
+                players: [socket.id, opponentId]
+            };
+
+            io.to(roomId).emit('matchFound', {
+                roomId
+            });
+
+        } else {
+
+            waitingPlayers.push(socket.id);
+
+            socket.emit('waitingForOpponent');
+        }
+    });
+
+    socket.on('disconnect', () => {
+
+        const index =
+            waitingPlayers.indexOf(socket.id);
+
+        if (index !== -1) {
+            waitingPlayers.splice(index, 1);
+        }
+
+        console.log('DISCONNECTED:', socket.id);
+    });
+});
+
+app.get('/', (_, res) => {
+    res.send('Tetris Multiplayer Server Running');
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+    console.log(`SERVER RUNNING ON ${PORT}`);
 });
