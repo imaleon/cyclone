e46ee7f123aa7e1f9f64c2ac32e943399b32ef84
Aleon <aneloaleon@gmail.com>
2026-05-19 10:50:57 +0800

    Add files via upload

--- server/server.js
+++ server/server.js
@@ -1,86 +1,93 @@
-const express = require('express');
-const cors = require('cors');
-const http = require('http');
-const { Server } = require('socket.io');
+// server.js
+
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
 
 const app = express();
 
-app.use(cors());
-
 const server = http.createServer(app);
 
-const io = new Server(server, {
-    cors: {
-        origin: '*'
-    }
+const io = new Server(server,{
+    cors:{origin:"*"}
 });
 
-const waitingPlayers = [];
-const rooms = {};
+let waiting = null;
 
-function createRoomId() {
-    return Math.random()
-        .toString(36)
-        .substring(2, 8)
-        .toUpperCase();
-}
+io.on("connection",socket=>{
 
-io.on('connection', socket => {
+    socket.on("findMatch",()=>{
 
-    console.log('CONNECTED:', socket.id);
+        if(waiting){
 
-    socket.on('findMatch', () => {
+            const room =
+                waiting.id + socket.id;
 
-        if (waitingPlayers.length > 0) {
+            waiting.join(room);
+            socket.join(room);
 
-            const opponentId = waitingPlayers.shift();
+            waiting.emit(
+                "matchFound",
+                room
+            );
 
-            const roomId = createRoomId();
+            socket.emit(
+                "matchFound",
+                room
+            );
 
-            socket.join(roomId);
+            waiting = null;
 
-            const opponentSocket =
-                io.sockets.sockets.get(opponentId);
+        }else{
 
-            if (!opponentSocket) return;
-
-            opponentSocket.join(roomId);
-
-            rooms[roomId] = {
-                players: [socket.id, opponentId]
-            };
-
-            io.to(roomId).emit('matchFound', {
-                roomId
-            });
-
-        } else {
-
-            waitingPlayers.push(socket.id);
-
-            socket.emit('waitingForOpponent');
+            waiting = socket;
         }
     });
 
-    socket.on('disconnect', () => {
+    socket.on("createRoom",room=>{
 
-        const index =
-            waitingPlayers.indexOf(socket.id);
+        socket.join(room);
 
-        if (index !== -1) {
-            waitingPlayers.splice(index, 1);
+        socket.emit(
+            "roomJoined",
+            room
+        );
+    });
+
+    socket.on("joinRoom",room=>{
+
+        const clients =
+            io.sockets.adapter.rooms.get(room);
+
+        if(clients && clients.size < 2){
+
+            socket.join(room);
+
+            io.to(room)
+                .emit("roomJoined",room);
         }
+    });
 
-        console.log('DISCONNECTED:', socket.id);
+    socket.on("board",data=>{
+
+        socket.to(data.room)
+            .emit(
+                "enemyBoard",
+                data.board
+            );
+    });
+
+    socket.on("garbage",data=>{
+
+        socket.to(data.room)
+            .emit(
+                "receiveGarbage",
+                data.garbage
+            );
     });
 });
 
-app.get('/', (_, res) => {
-    res.send('Tetris Multiplayer Server Running');
-});
-
-const PORT = process.env.PORT || 3000;
-
-server.listen(PORT, () => {
-    console.log(`SERVER RUNNING ON ${PORT}`);
-});
+server.listen(
+    process.env.PORT || 3000,
+    ()=>console.log("Server running")
+);
